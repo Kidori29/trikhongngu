4fafccd68f6652b805f111248781879eae28b06f
Kyrux.29 <vutrongquockhanh29@gmail.com>
2025-07-22 23:00:55 +0700

    Update include.js

--- assets/js/include.js
+++ assets/js/include.js
@@ -1,14 +1,14 @@
 // assets/js/include.js
 document.addEventListener('DOMContentLoaded', function() {
   // Load header
-  fetch('../../partials/header.html')
+  fetch('partials/header.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('include-header').innerHTML = data;
     });
 
   // Load footer
-  fetch('../../partials/footer.html')
+  fetch('partials/footer.html')
     .then(response => response.text())
     .then(data => {
       document.getElementById('include-footer').innerHTML = data;
